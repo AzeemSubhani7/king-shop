0a4746533cf47a576920f376d11393d82ca1ceca
Azeem Subhani <azeemforall4406148@gmail.com>
2021-07-05 04:03:02 +0500

    adding functionality to add user through email and password xD

--- src/pages/signin-signup/SignInSignUp.js
+++ src/pages/signin-signup/SignInSignUp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SignInSignUp.scss';
 import SignIn from '../../components/signIn/signIn';
+import SignUp from '../../components/signup/signup';
 
 const SignInSignUp = () => {
     return(
         <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
