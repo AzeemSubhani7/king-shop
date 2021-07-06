b8a951487df810e951b01b274a4035307553ca56
Azeem Subhani <azeemforall4406148@gmail.com>
2021-07-07 00:58:03 +0500

    fully implemented cart dropdown

--- src/components/cartdropdown/cartDropdown.js
+++ src/components/cartdropdown/cartDropdown.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Button from '../button/button'
+import CartItem from '../cartItem/cartItem'
 
 import './cartDropdown.scss'
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return(
         <div className='cart-dropdown'>
-            <div className='cart=item'></div>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: {cartItems} }) => {
+    return {
+        cartItems
+    }
+}
+
+export default connect(mapStateToProps)(CartDropdown);
